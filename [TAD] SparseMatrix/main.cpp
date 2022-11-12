cf5dd05e98b73e18ce88b1e91822c1f8c89f2d22
Kalmax dos Santos Sousa <kalmax.santos@enivixlabs.com.br>
2022-11-12 10:44:43 -0300

    Tratando exceções

--- [TAD] SparseMatrix/main.cpp
+++ [TAD] SparseMatrix/main.cpp
@@ -8,75 +8,90 @@
 #include <fstream>
 
 
-SparseMatrix* readSparseMatrix(std::string filename){
+SparseMatrix* readSparseMatrix(std::string filename){  //O(n²)
     std::ifstream file(filename);
-    int rows, cols, value, row, col;
+    int rows, cols;
+    double value;
+
     file >> rows >> cols;
     SparseMatrix* matrix = new SparseMatrix(rows, cols);
-    for (int i = 1; i <= rows; i++)
-    {
-        for (int j = 1; j <= cols; j++)
-        {
+    for (int i = 1; i <= rows; i++){
+        for (int j = 1; j <= cols; j++){
             file >> value;
             matrix->insert(i, j, value);
-            /* code */
         }
     }
     return matrix;
 }
 
-SparseMatrix* sum(SparseMatrix* A, SparseMatrix* B){
-    if(A->getRows() == B->getRows() && A->getColumns() == B->getColumns()){
-        SparseMatrix* aux = new SparseMatrix(A->getRows(), A->getColumns()); // Cria uma matriz auxiliar
-        for(int i =1; i <= A->getRows(); i++){ // Percorre as linhas
-            for(int j = 1; j <= A->getColumns(); j++){  // Percorre as colunas
-                aux->insert(i, j, A->get(i, j) + B->get(i, j));  // Insere o valor da soma na matriz auxiliar
+SparseMatrix* sum(SparseMatrix* A, SparseMatrix* B){ //O(n²)
+        if(A->getRows() == B->getRows() && A->getColumns() == B->getColumns()){
+            SparseMatrix* aux = new SparseMatrix(A->getRows(), A->getColumns()); // Cria uma matriz auxiliar
+            for(int i =1; i <= A->getRows(); i++){ // Percorre as linhas
+                for(int j = 1; j <= A->getColumns(); j++){  // Percorre as colunas
+                    aux->insert(i, j, A->get(i, j) + B->get(i, j));  // Insere o valor da soma na matriz auxiliar
+                }
             }
+            return aux; // Retorna a matriz auxiliar
         }
-        return aux; // Retorna a matriz auxiliar
-    }
+        else{
+            throw std::runtime_error("As matrizes não podem ser somadas");
+        }
 }
 
 
-SparseMatrix* multiply(SparseMatrix* A, SparseMatrix* B){
+SparseMatrix* multiply(SparseMatrix* A, SparseMatrix* B){ //O(n²)
     if(A->getColumns() == B->getRows()){
         SparseMatrix* aux = new SparseMatrix(A->getRows(), B->getColumns());
 
         for (int i = 1; i <= aux->getRows(); i++){
             for (int j = 1; j <= aux->getColumns(); j++){
-                int temp = 0;
+                double temp = 0;
                 for (int k = 1; k <= A->getColumns(); k++){
                     temp += A->get(i, k) * B->get(k, j);
-                }
-                
+                }       
                 aux->insert(i, j, temp);
             }
         }
-
         return aux;
+    }
+
+    else{
+        throw std::runtime_error("As matrizes não podem ser multiplicadas");
     }
 }
 
 
 int main(){
-   
-   
     SparseMatrix* A = readSparseMatrix("A.txt");
-    SparseMatrix* B = readSparseMatrix("B.txt");
-    SparseMatrix* A1 = readSparseMatrix("A.txt");
-    
-    SparseMatrix* C = sum(A, A1);
-    SparseMatrix* D = multiply(A, B);
     std::cout << "A:" << std::endl;
     A->print();
+
+    SparseMatrix* B = readSparseMatrix("B.txt");
     std::cout << "B:" << std::endl;
     B->print();
-    std::cout << "C:" << std::endl;
-    C->print();
-    std::cout << "D:" << std::endl;
-    D->print();
+
+    SparseMatrix* C;
+    try{
+        C = sum(A, B);
+        std::cout << "C:" << std::endl;
+        C->print();
+
+    }catch(const std::runtime_error ex){
+        std::cout << ex.what() << std::endl;
+    }
+
+
+    SparseMatrix* D;
+    try{
+        D = multiply(A, B);
+        std::cout << "D:" << std::endl;
+        D->print();
+
+    }catch(const std::runtime_error ex){
+        std::cout << ex.what() << std::endl;
+    }
+
     return 0;
-   std :: cout << B ->get(1, 2) << std::endl;
-   B->print();
    
 }
